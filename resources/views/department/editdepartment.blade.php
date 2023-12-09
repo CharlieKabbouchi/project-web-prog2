70d753281e8fb8116fa75d26d35d27417a65a83b
Charlie El Kabbouchi <charlie.el.kabbouchi@gmail.com>
2023-12-10 01:46:53 +0200

    wip

--- resources/views/department/editdepartment.blade.php
+++ resources/views/department/editdepartment.blade.php
@@ -1,45 +1,28 @@
 @extends('layout.admin')
 <title>Admin Departments</title>
-<style>
-    .actions-column {
-        display: flex;
-        flex-direction: row;
-        gap: 5px;
-    }
-  
-</style>
-
 
 @section('content')
-    <div class="row">
-        <h4 class="page-title">{{$department->name}} Department</h4>
-    </div>
-   
-    <div class="row">
-        <form method="post" action="{{ route('department.update', ['department' => $department->id]) }}">
-            @csrf
-            @method('PUT') 
-    
-            <div class="form-group">
-                <label for="name">Department Name:</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $department->name }}" required>
-            </div>
-    
-            <div class="form-group">
-                <label for="location">Location:</label>
-                <input type="text" class="form-control" id="location" name="location" value="{{ $department->location }}" required>
-            </div>
-    
-            <div class="form-group">
-                <label for="totalCredits">Total Credits:</label>
-                <input type="number" class="form-control" id="totalCredits" name="totalCredits" value="{{ $department->totalCredits }}" required>
-            </div>
-    
-            <button type="submit" class="btn btn-primary">Update Department</button>
-        </form>
-    </div>
+<div class="row">
+    <form method="post" action="{{ route('admin.updatedepartment', ['department' => $department->id]) }}">
+        @csrf
+        @method('PUT') <!-- Use 'PUT' method for updating -->
 
-   
-    </div>
-                   
+        <div class="form-group">
+            <label for="name">Department Name:</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ $department->name }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="location">Location:</label>
+            <input type="text" class="form-control" id="location" name="location" value="{{ $department->location }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="totalCredits">Total Credits:</label>
+            <input type="number" class="form-control" id="totalCredits" name="totalCredits" value="{{ $department->totalCredits }}" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Update Department</button>
+    </form>
+</div>
 @endsection('content')
