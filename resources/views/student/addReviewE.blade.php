508ac4e893af436bdfbd90a6704e681bcb0c6bbd
Charlie El Kabbouchi <charlie.el.kabbouchi@gmail.com>
2023-12-15 04:51:25 +0200

    view fix

--- resources/views/student/addReviewE.blade.php
+++ resources/views/student/addReviewE.blade.php
@@ -2,21 +2,27 @@
 <title>Add Review to Event</title>
 
 @section('content')
-    <div class="row">
-        <h5>Add Review to Event</h5>
+        <div class="row">
+            <div class="col-md-12">
+                <h5>Add Review to Event</h5>
 
-        <p>Event Title: {{ $event->title }}</p>
-        <p>Event Description: {{ $event->description }}</p>
+                <p>Event Title: {{ $event->title }}</p>
+                <p>Event Description: {{ $event->description }}</p>
 
-        <form method="post" action="{{ route('student.submitReviewE', ['eventId' => $event->id]) }}">
-            @csrf
-            <label for="description">Review Description:</label>
-            <textarea name="description" id="description" rows="4" required></textarea>
+                <form method="post" action="{{ route('student.submitReviewE', ['eventId' => $event->id]) }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="description">Review Description:</label>
+                        <textarea class="form-control" name="description" id="description" rows="4" required></textarea>
+                    </div>
 
-            <label for="rating">Rating:</label>
-            <input type="number" name="rating" id="rating" min="1" max="5" required>
+                    <div class="form-group">
+                        <label for="rating">Rating:</label>
+                        <input class="form-control" type="number" name="rating" id="rating" min="1" max="5" required>
+                    </div>
 
-            <button type="submit">Submit Review</button>
-        </form>
-    </div>
-@endsection
+                    <button type="submit" class="btn btn-primary">Submit Review</button>
+                </form>
+            </div>
+        </div>
+    @endsection
--- resources/views/student/addreviewc.blade.php
+++ resources/views/student/addreviewc.blade.php
@@ -2,19 +2,23 @@
 <title>Add Review to Class</title>
 
 @section('content')
-    <div class="row">
+<div class="row">
+    <div class="col-md-12">
         <h5>Add Review to this Class</h5>
-            <div class="col-md-12">
-                <form method="post" action="{{ route('student.storereviewc', ['classId' => $classId]) }}">
-                    @csrf
-                    <label for="description">Review Description:</label>
-                    <textarea name="description" id="description" rows="4" required></textarea>
-    
-                    <label for="rating">Rating:</label>
-                    <input type="number" name="rating" id="rating" min="1" max="5" required>
-    
-                    <button type="submit">Submit Review</button>
-                </form>
+        <form method="post" action="{{ route('student.storereviewc', ['classId' => $classId]) }}">
+            @csrf
+            <div class="form-group">
+                <label for="description">Review Description:</label>
+                <textarea class="form-control" name="description" id="description" rows="4" required></textarea>
             </div>
+
+            <div class="form-group">
+                <label for="rating">Rating:</label>
+                <input class="form-control" type="number" name="rating" id="rating" min="1" max="5" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit Review</button>
+        </form>
     </div>
+</div>
 @endsection
